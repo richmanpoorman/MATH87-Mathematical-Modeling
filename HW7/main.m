9a0c8910d4e2d4917f39c06a9b34d4d74816530e
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2024-12-05 22:38:41 -0500

    fixes to solving

--- HW7/main.m
+++ HW7/main.m
@@ -20,11 +20,11 @@ function coefficients = fitConicSection(points)
         equation_row = [x * x, x * y, y * y, x, y];
         equations = [equations ; equation_row];
     end
-
-    coefficients = equations \ result; 
+    equations_t = equations.'; 
+    coefficients = (equations_t * equations) \ (equations_t * result); 
 end
 
-function graph_contour(coefficients, points, x_range, y_range)
+function graph_contour(coefficients, points, x_range, y_range, figure_index)
     a = coefficients(1); 
     b = coefficients(2); 
     c = coefficients(3); 
@@ -32,7 +32,7 @@ function graph_contour(coefficients, points, x_range, y_range)
     e = coefficients(5); 
 
     fprintf('a: %f \nb: %f \nc: %f \nd: %f \ne: %f \n', a, b, c, d, e);
-
+    fig = figure(figure_index);
     [X,Y] = meshgrid(x_range, y_range);
     f = ones(size(X));
     Z = a*X.^2 + b*X.*Y + c*Y.^2 + d*X + e*Y + f;
@@ -57,15 +57,15 @@ dataset_1_y_range = -5 : 0.1 : 8;
 dataset_1_points = [dataset_1.x, dataset_1.y]; 
 
 dataset_1_coefficients = fitConicSection(dataset_1_points);
-graph_contour(dataset_1_coefficients, dataset_1_points, dataset_1_x_range, dataset_1_y_range);
+graph_contour(dataset_1_coefficients, dataset_1_points, dataset_1_x_range, dataset_1_y_range, 1);
 
 fprintf('- DATASET 2 \n');
-dataset_2_x_range =  -2 : 0.1 : 7; 
-dataset_2_y_range = -5 : 0.1 : 8; 
+dataset_2_x_range =  -4 : 0.1 : 2; 
+dataset_2_y_range = -1 : 0.1 : 5; 
 dataset_2_points = [dataset_2.x, dataset_2.y]; 
 
 dataset_2_coefficients = fitConicSection(dataset_2_points);
-graph_contour(dataset_2_coefficients, dataset_2_points, dataset_2_x_range, dataset_2_y_range);
+graph_contour(dataset_2_coefficients, dataset_2_points, dataset_2_x_range, dataset_2_y_range, 2);
 
 
 %%% 
